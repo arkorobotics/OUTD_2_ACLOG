c60d1f46c2cde16c6e5008d10864fb70489489ea
Arko <dotuplink@gmail.com>
2021-05-01 20:21:30 -0700

    Added a custom comment feature and build instructions
    Added a custom comment feature and build instructions

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# OUTD 2 ACLOG
+# OUTD-2-ACLOG
 
 Converts OutD ADIF log files to ACLOG (N3FJP) ADIF parseable log files
+
+## Build Dependencies
+
+OUTD-2-ACLOG is typically used on Windows machines since ACLOG is a Windows-only program, but it can run on any machine which supports Python.
+
+1.  [Python](https://www.python.org/downloads/) 
+2. [Pip](https://pip.pypa.io/en/stable/installing/)
+3. Tkinter
+```
+pip install tk
+```
+4. (OPTIONAL) To build an exe for Windows, you will need [Pyinstaller](https://www.pyinstaller.org/downloads.html)
+
+## Run
+```
+> python outd2aclog.py
+```
+
+## Build Window EXE
+
+```
+pyinstaller --onefile --noconsole --icon outd2aclogicon.ico outd2aclog.py
+```
--- outd2aclog.py
+++ outd2aclog.py
@@ -26,60 +26,60 @@ def openFile(input_filename):
     try:
         input_filename.index("OutdLog-")
     except ValueError:
-        print("Invalid file!")
+        label_file_explorer.configure(text="  Invalid File!        ")
     else:
         output_filename = input_filename.replace("OutdLog-", "ACLOG-")
         fout = open(output_filename, 'w+')
 
-        replace_string = '<OTHER:4>' + other_var.get() + '<MY_GRIDSQUARE:4>' + grid_var.get() + '<EOR>'
-
         for line in fin:
-            new_line = line.replace('QSPMSG', 'COMMENT')
-            new_line = line.replace('<EOR>', replace_string)
+
+            if len(comment_var.get()) == 0:
+                new_line = line.replace('QSPMSG', 'COMMENT')
+                new_line = line.replace('<EOR>', '<OTHER:4>' + other_var.get() + '<MY_GRIDSQUARE:4>' + grid_var.get() + '<EOR>')
+            else:
+                new_line = line.replace('<EOR>', '<OTHER:4>' + other_var.get() + '<MY_GRIDSQUARE:4>' + grid_var.get() + '<COMMENT:' + str(len(comment_var.get())) + '>' + comment_var.get() + '<EOR>')
 
             fout.write(new_line)
         
         fout.close()
 
-    fin.close()
+        label_file_explorer.configure(text="  Converted successfully!        ")
 
-    label_file_explorer.configure(text="  Converted successfully!        ")
+    fin.close()
 
 window = Tk()
 window.resizable(0, 0)
 
+## If you wish to build an executable with the OUTD-2-ACLOG icon, uncomment and modify the following two lines:
+# icon = PhotoImage(file = r"[absolute file path here]\outd2aclogicon.png")
+# window.iconphoto(False, icon)
+
 grid_var = StringVar()
 other_var = StringVar()
 other_var.set( "SOTA" )
+comment_var = StringVar()
 
 window.title('OUTD 2 ACLOG')
-  
-window.config(background='#3E4149')
 
 label_file_explorer = Label(window,
-                            text = "Select OutD ADIF file:           ",
-                            fg = "white",
-                            background='#3E4149')
+                            text = "Select OutD ADIF file:")
 
 button_explore = Button(window,
-                        text = "Browse Files", highlightbackground='#3E4149',
+                        text = "Browse Files",
                         command = browseFiles)
 
-grid_label = Label(window, text = 'My 4-Digit Gridsqure: ', fg = "white", background='#3E4149')
-grid_entry = Entry(window, textvariable = grid_var, highlightbackground='#3E4149')
+grid_label = Label(window, text = 'My 4-Digit Gridsqure: ')
+grid_entry = Entry(window, textvariable = grid_var)
       
-otherfield_label = Label(window, text = 'Type: ', fg = "white", background='#3E4149')
+otherfield_label = Label(window, text = 'Type: ',)
 otherfield_drop = OptionMenu( window , other_var, "SOTA", "CHASE")
-otherfield_drop.config(background='#3E4149')
-otherfield_drop.pack()
+
+comment_label = Label(window, text = 'Comment: ')
+comment_entry = Entry(window, textvariable = comment_var)
 
 button_convert = Button(window,
-                     text = "Convert", highlightbackground='#3E4149',
+                     text = "Convert",
                      command = lambda: openFile(filename_var))
-
-button_exit = Button(window,
-                     text = "Exit", highlightbackground='#3E4149',
-                     command = exit)
   
 label_file_explorer.grid(row = 1, column = 0, sticky=E)
 
@@ -91,7 +91,9 @@ grid_entry.grid(row=3, column=1, sticky=W)
 otherfield_label.grid(row = 4, column = 0, sticky=E)
 otherfield_drop.grid(row = 4, column = 1, sticky=W)
 
-button_convert.grid(row = 5, column = 1)
-button_exit.grid(row = 5, column = 0)
+comment_label.grid(row=5,column= 0, sticky=E)
+comment_entry.grid(row=5, column=1, sticky=W)
+
+button_convert.grid(row = 6, column = 1)
 
 window.mainloop()
